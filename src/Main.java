41d1ad1ffc5474d197c3b6b4d56241de2c22b56f
PatriosLLC <Nathaniel.Keating@Patrios-us.com>
2026-08-28 14:51:44 -0400

    ingore case and ending punctuation; removed UTF-8 apostrophes

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 
 public class Main {
     final static String[] sonnet = {
-        "Shall", "I", "compare", "thee", "to", "a", "summer’s", "day?", "\n",
+        "Shall", "I", "compare", "thee", "to", "a", "summer's", "day?", "\n",
         "Thou", "art", "more", "lovely", "and", "more", "temperate:", "\n",
         "Rough", "winds", "do", "shake", "the", "darling", "buds", "of", "May,", "\n",
-        "And", "summer’s", "lease", "hath", "all", "too", "short", "a", "date;", "\n",
+        "And", "summer's", "lease", "hath", "all", "too", "short", "a", "date;", "\n",
         "Sometime", "too", "hot", "the", "eye", "of", "heaven", "shines,", "\n",
-        "And", "often", "is", "his", "gold", "complexion", "dimm’d;", "\n",
+        "And", "often", "is", "his", "gold", "complexion", "dimm'd;", "\n",
         "And", "every", "fair", "from", "fair", "sometime", "declines,", "\n",
-        "By", "chance", "or", "nature’s", "changing", "course", "untrimm'd;", "\n",
+        "By", "chance", "or", "nature's", "changing", "course", "untrimm'd;", "\n",
         "But", "thy", "eternal", "summer", "shall", "not", "fade,", "\n",
-        "Nor", "lose", "possession", "of", "that", "fair", "thou", "ow’st;", "\n",
-        "Nor", "shall", "death", "brag", "thou", "wander’st", "in", "his", "shade,", "\n",
-        "When", "in", "eternal", "lines", "to", "time", "thou", "grow’st:", "\n",
+        "Nor", "lose", "possession", "of", "that", "fair", "thou", "ow'st;", "\n",
+        "Nor", "shall", "death", "brag", "thou", "wander'st", "in", "his", "shade,", "\n",
+        "When", "in", "eternal", "lines", "to", "time", "thou", "grow'st:", "\n",
         "\t", "So", "long", "as", "men", "can", "breathe", "or", "eyes", "can", "see,", "\n",
         "\t", "So", "long", "lives", "this,", "and", "this", "gives", "life", "to", "thee."
     };
@@ -50,7 +50,7 @@ public class Main {
             System.out.println("_____ ");
             System.out.print("Enter the next word: ");
             String userWord = scanner.nextLine();
-            if (userWord.equals(sonnet[wordToStop].replaceAll("[;:]$", ""))) {
+            if (userWord.equalsIgnoreCase(sonnet[wordToStop].replaceAll("[,.?;:]$", ""))) {
                 System.out.println("Correct!");
                 numCorrect++;
             }
